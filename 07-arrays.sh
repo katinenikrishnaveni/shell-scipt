2a536b9f0808201fc10ad14187805faa966a0192
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-01-28 13:01:03 +0530

    Script added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,4 +3,4 @@ Movies=("RRR" "Game changer" "Bhahubali")
 echo "First movie:"$Movies[0]
 echo "Second movie:"$Movies[1]
 echo "Third movie:"$Movies[3]
-echo "All movies1:"$Mvoies[@]
+echo "All movies:"$Movies[@]
