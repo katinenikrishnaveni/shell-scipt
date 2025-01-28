846368495483dd49802e7693ec295453bfb1d56d
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-01-28 13:13:08 +0530

    Script added

---
+++ 08-special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables display :$@"
+echo "Number of variables :$#"
+echo "Current Script Name:$0"
+echo "current working directory:$PWD"
+echo "home directory of current user:$HOME"
+echo "which user is running the script:$USER"
+echo "Procecc id of current script:$$"
+sleep 60
+echo "procecc id of last command in background:$!"
