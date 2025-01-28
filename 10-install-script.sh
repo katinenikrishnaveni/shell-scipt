c9b1781ba103ec6b71edc3bbcc165ed5419522ff
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-01-28 17:04:37 +0530

    Script added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,21 +5,34 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "ERROR:: you must have to ecute the sudo access"
+   exit 1
 fi
-
-dnf install mysqld -y
- if [ $? -ne 0 ]
-  then 
+dnf list installed mysql
+if [$? -ne 0]
+then
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+   then 
       echo "Installing Mysql.....failure"
+      exit 1
    else
       echo "Installing Mysql.....success"
- fi
+   fi
+else
+    echo "Mysql is already Installed"
+fi
 
- dnf install git -y
-
- if [ $? -ne 0 ]
-  then 
+dnf list installed git
+if [$? -ne 0 ]
+then 
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
      echo "Installing Git.....Failure"
+     exit 1
     else
      echo "Installing git.....Success"
- fi
+    fi
+else
+    echo "Git is alredy installed"
+fi        
