8656d3f96f31702d55a71e211a90d994668516d0
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-01-28 12:57:33 +0530

    Script added

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -6,4 +6,4 @@ Number2=$2
 sum=$(($Number1+$Number2))
 echo "sum of $Number1 and $Number2 is:$sum"
 Time=$(date)
-echo "Current time display:$Time"
+echo "Script timing:$Time"
---
+++ 07-arrays.sh
@@ -0,0 +1,6 @@
+Movies=("RRR" "Game changer" "Bhahubali")
+
+echo "First movie:$Movies[0]"
+echo "Second movie:$Movies[1]"
+echo "Third movie:$Movies[3]"
+echo "All movies1:$Mvoies[@]"
