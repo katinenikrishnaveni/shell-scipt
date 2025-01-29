834657277f55f8f0d30f05a23013d1483ec4d3dc
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-01-29 14:08:17 +0530

    script added

---
+++ 11-functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=&(id -u)
+if [ $USERID -ne 0 ]
+then 
+   echo "ERROR::you must have to enter the sudo access"
+fi
+validate(){
+    if [$1 -ne 0]
+    then
+       echo "$2 ......failure"
+    else 
+       echo "$2.......Success"
+    fi      
+}
+
+dnf list installed mysql 
+ if [$? -ne 0]
+ then
+     dnf install mysql -y
+     validate() $? Installing 
+ else
+    echo "Mysql is already installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    validate() $? Installing
+else
+    echo "Git is already installed"
+fi                
