6c62ee90b705e5a31e49cf49a409060d492a9177
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-02-05 11:54:56 +0530

    script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -21,27 +21,27 @@ validate()
   fi  
 }   
 
-echo "Script excution started at::$Timestamp"&>>Log_File_Name
+echo "Script excution started at::$Timestamp" &>>$Log_File_Name
 if [$USERID -ne 0]
 then
    echo "ERROR::you have to acess sudo"
    exit 1
 fi
 
-dnf list installed mysql&>>Log_File_Name
+dnf list installed mysql &>>$Log_File_Name
 if [$? -ne 0]
 then  
-   dnf install mysql -y &>>Log_File_Name
+   dnf install mysql -y &>>$Log_File_Name
    validate $? "Installing mysql"
 else
    echo -e "Already installed.....$Y mysql $N"
 fi
 
-dnf list installed git &>>Log_File_Name
+dnf list installed git &>>$Log_File_Name
 
 if [$? -ne 0]
 then  
-    dnf install git -y &>>Log_File_Name
+    dnf install git -y &>>$Log_File_Name
     validate $? "Installing Git"
 else
     echo -e "Alredy installed.......$Y git $N" 
