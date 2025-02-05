97aadd796b8e4cbb61f2adf840f93130f58e0897
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-02-05 12:21:28 +0530

    script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -12,7 +12,7 @@ Log_File_Name="$logs_folder/$log_file-$Timestamp.log"
 
 validate()
 {
- if [$1 -ne 0]
+ if [ $1 -ne 0 ]
  then
      echo -e "$2.....$R Failure $N"
      exit 1
@@ -23,7 +23,7 @@ validate()
 
 echo "Script excution started at::$Timestamp" &>>$Log_File_Name
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
    echo "ERROR::you have to acess sudo"
    exit 1
@@ -31,7 +31,7 @@ fi
 
 dnf list installed mysql &>>$Log_File_Name
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then  
    dnf install mysql -y &>>$Log_File_Name
    validate $? "Installing mysql"
@@ -41,7 +41,7 @@ fi
 
 dnf list installed git &>>$Log_File_Name
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then  
     dnf install git -y &>>$Log_File_Name
     validate $? "Installing Git"
