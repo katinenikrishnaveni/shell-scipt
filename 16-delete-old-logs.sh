41fe78ceaedd59ae74a63b8161ceccc09a40243c
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-02-10 17:34:48 +0530

    script added

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -34,3 +34,9 @@ fi
 file_to_delete=$(find $source_id -name "*.log" -mtime +14)
 echo "file is to be deleted :$file_to_delete"
 
+while read -r file
+do
+ echo "delete the file:$file"
+ rm -rf $file
+done <<< $file_to_delete 
+
