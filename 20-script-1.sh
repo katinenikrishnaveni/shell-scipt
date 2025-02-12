4e56df8b48a0ec734d39b1a9d0816f7e2190067c
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-02-12 17:00:53 +0530

    script added

--- 20-script-1.sh
+++ 20-script-1.sh
@@ -4,5 +4,6 @@ msg="script1"
 GREET="Hi from script-1"
 source ./21-script-2.sh
 echo " Hello:$msg"
+echo " value of:$A"
 
 #sh 21-script-2.sh
--- 21-script-2.sh
+++ 21-script-2.sh
@@ -4,3 +4,5 @@ msg1="script2"
 
 echo "Hi from:$msg1"
 echo "Greeting from:$GREET"
+
+A=10
