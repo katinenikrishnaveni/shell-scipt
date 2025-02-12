195e130f638850d88cbff74f8881dbce11302378
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-02-12 16:55:28 +0530

    script added

--- 20-script-1.sh
+++ 20-script-1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 msg="script1"
-
+GREET="Hi from script-1"
+source ./21-script-2.sh
 echo " Hello:$msg"
 
-sh 21-script-2.sh
+#sh 21-script-2.sh
--- 21-script-2.sh
+++ 21-script-2.sh
@@ -3,3 +3,4 @@
 msg1="script2"
 
 echo "Hi:$msg1"
+echo "Greeting from:$GREET"
