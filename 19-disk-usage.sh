1cd7f85f2c1939e75529386db3806ff469d062a7
katineni krishanveni <krishnakrishnaveni546@gmail.com>
2025-02-12 16:14:40 +0530

    script added

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,13 +2,13 @@
 
 DISK_USAGE=$(df -hT|grep xfs)
 DISK_THESHOLD=5
-MSG=""
+MSG=" "
 
 while read -r line
 do
   UASGE=$(echo $line |awk -F " " '{print $6F}'|cut -d "%" -f1)
   PARTITION=$(echo $line|awk -F " " '{print $NF}')
   MSG="High disk usage of partition:$PARTITION usage of:$UASGE"
-done <<<$DISK_USAGE
+done <<< $DISK_USAGE
 
-echo "Message"$MSG"
+echo "Message:$MSG"
